d9732cf0926a17692265b198a9ab2d5a2d95e9e8
KUssaoT <georgehasballs12@gmail.com>
2026-09-02 18:03:16 +0100

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -343,7 +343,7 @@ if (SONG.song.toLowerCase() == 'thunderstorm')
 
 	var backMount:FlxSprite = new FlxSprite(-700, -40).loadGraphic(Paths.image('backgrounds/thunda/backmount'));
 	backMount.scrollFactor.set(0.4, 1);
-	backMount.setGraphicSize(Std.int(ground.width * 1.7));
+	backMount.setGraphicSize(Std.int(backMount.width * 1.7));
 	add(backMount);
 
 	var middleMount:FlxSprite = new FlxSprite(-240, 200).loadGraphic(Paths.image('backgrounds/thunda/middlemount'));
